9fdaeaac704234e1d73efaa287c1895b7f6fcc38
debasish-max <hazarikadebasish182@gmail.com>
2026-02-06 23:48:30 +0530

    home page ui changed

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -10,17 +10,23 @@ export default function ProductCard({ product, setToast }) {
   };
 
   return (
-    <div className="bg-white rounded-xl p-4 shadow">
+    <div className="bg-white rounded-xl p-3 shadow">
       <img
         src={product.img}
-        className="h-48 w-full object-cover rounded-lg"
+        className="h-32 w-full object-cover rounded-lg"
       />
-      <h3 className="mt-2 font-semibold">{product.name}</h3>
-      <p className="text-brand font-bold">₹{product.price}</p>
+
+      <h3 className="mt-1 text-sm font-semibold leading-tight">
+        {product.name}
+      </h3>
+
+      <p className="text-brand font-bold text-sm">
+        ₹{product.price}
+      </p>
 
       <button
         onClick={handleAdd}
-        className="mt-3 w-full bg-brand text-white py-2 rounded-full"
+        className="mt-2 w-full bg-brand text-white py-1.5 text-sm rounded-full"
       >
         Add to Cart
       </button>
